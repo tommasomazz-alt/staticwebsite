62eb1822b59afa61980f97a9802923ba9098e3ce
tommasomazz-alt <tommaso.mazz@gmail.com>
2026-08-13 20:46:11 +0200

    Added BlockType enum and function to identify block type from starting md block

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = markdown.split("\n\n")
     result = []
@@ -7,3 +9,42 @@ def markdown_to_blocks(markdown: str) -> list[str]:
             result.append(block)
 
     return result
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(block: str) -> BlockType:
+    lines = block.split("\n")
+
+    if block.startswith(("#","##","###","####","#####","######")):
+        return BlockType.HEADING
+
+    if len(lines) >1 and block.startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,9 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (
+    markdown_to_blocks, 
+    block_to_block_type,
+    BlockType
+)
 
 class TestMarkdown(unittest.TestCase):
 
@@ -48,3 +52,53 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_blocks_paragraph(self):
+        md = "This is a normal paragraph"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_blocks_heading(self):
+        md = "#HEADER 1"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_blocks_code(self):
+        md = "```\nThis is a code paragraph\n```"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_WRONG_blocks_code(self):
+        md = "```\nThis is a code paragraph\n``"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_blocks_quote(self):
+        md = ">Cogito ergo sum"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_WRONG_blocks_quote(self):
+        md = ">Cogito ergo sum\n>Alea iacta est\nMa io che cazzo ne so"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_blocks_unordered_list(self):
+        md = "- item \n- another item 2\n- one more item"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_WRONG_blocks_unordered_list(self):
+        md = "- item \n- another item 2\n- one more item\nfanculo gli elenchi"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_blocks_ordered_list(self):
+        md = "1. item one\n2. item two\n3. item three"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_WRONG_blocks_ordered_list(self):
+        md = "1. item one\n2. item two\n3. item three\nD. item dee"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
